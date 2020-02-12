2053be1e07876781563207d6754f3583886faa6e
J. Farid <jrab227@gmail.com>
2020-02-12 08:03:16 -0500

    Prep heroku

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
+
+app.use(express.static('dist'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
