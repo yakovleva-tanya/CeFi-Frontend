5a8c0a8cd5012d99b6076b0da7617820b3e57834
J. Farid <jrab227@gmail.com>
2020-03-27 12:24:51 -0400

    Remove local server.

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const plaidRouter = require('./api/plaid');
-
 app.use(cors());
 
 app.use(express.static('dist'));
-
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-app.use('/plaid', plaidRouter.router);
 
 app.listen(port, () => {
   console.log(`Listening on port ${ port }`);
