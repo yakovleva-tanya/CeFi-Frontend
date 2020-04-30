c2b14142f4165f3b9e7d40d6fa5d473b19bf6360
J. Farid <jrab227@gmail.com>
2020-04-30 16:15:42 -0400

    Adding dynamic check for decimals.

--- src/actions/lendDai.tsx
+++ src/actions/lendDai.tsx
@@ -3,7 +3,7 @@ import { globalDecimals } from "./../util/constants";
 
 export default async (amount: number, primaryAddress: string, lendingPoolContract: any, zDaiContract: any, web3State: any): Promise<any> => {
   await approveDai(lendingPoolContract, web3State, primaryAddress, amount);
-  await mintZDai(lendingPoolContract, primaryAddress, amount);
+  await mintZDai(lendingPoolContract, web3State, primaryAddress, amount);
   const balance = await zDaiContract.methods.balanceOf(primaryAddress).call();
   return (parseFloat(balance) / globalDecimals);
 }
--- src/models/Contracts.tsx
+++ src/models/Contracts.tsx
@@ -1,15 +1,20 @@
 import Notify from "./Web3Notify";
-import { globalDecimals } from "./../util/constants";
-import ERC20 = require('./../abi/contracts/ERC20.json');
+import ERC20 = require('./../abi/contracts/ERC20Detailed.json');
+
+async function getLendingToken(lendingPool: any, web3State: any) {
+  const lendingTokenAddress = await lendingPool.methods.lendingToken().call();
+  return new web3State.web3.eth.Contract(ERC20.abi, lendingTokenAddress, {});
+}
 
 /**
  * Approves spending of dai for zeroCollateral contracts.
  */
 export async function approveDai(lendingPool: any, web3State: any, primaryAddress: string, amount: number) {
-  const lendingTokenAddress = await lendingPool.methods.lendingToken().call();
-  const dai = new web3State.web3.eth.Contract(ERC20.abi, lendingTokenAddress, {});
+  const dai = await getLendingToken(lendingPool, web3State);
+  const decimals = await dai.methods.decimals().call();
+  const tokenDecimals = 10**parseFloat(decimals);
   const allowance = await dai.methods.allowance(primaryAddress, lendingPool._address).call();
-  const approved = (amount*globalDecimals) < parseFloat(allowance);
+  const approved = (amount*tokenDecimals) < parseFloat(allowance);
   if (approved) return;
   return new Promise((resolve, reject) => dai.methods
     .approve(
@@ -23,9 +28,12 @@ export async function approveDai(lendingPool: any, web3State: any, primaryAddres
   );
 }
 
-export function mintZDai(contract: any, primaryAddress: string, amount: number) {
+export async function mintZDai(contract: any, web3State: any, primaryAddress: string, amount: number) {
+  const dai = await getLendingToken(contract, web3State);
+  const decimals = await dai.methods.decimals().call();
+  const tokenDecimals = 10**parseFloat(decimals);
   return new Promise((resolve, reject) => contract.methods.deposit(
-      (globalDecimals*amount).toLocaleString('fullwide', { useGrouping:false })
+      (tokenDecimals*amount).toLocaleString('fullwide', { useGrouping:false })
     )
     .send({ from: primaryAddress })
     .on('transactionHash', Notify.hash)
