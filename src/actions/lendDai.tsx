4361c1ec81c6b1681013a5af2ffd15645bc19b81
J. Farid <jrab227@gmail.com>
2020-04-29 22:34:52 -0400

    Adding fixes for lendDai

--- src/actions/lendDai.tsx
+++ src/actions/lendDai.tsx
@@ -1,11 +1,10 @@
 import { approveDai, mintZDai } from "./../models/Contracts";
 
-import globalDecimals from "./../util/constants";
+import { globalDecimals } from "./../util/constants";
 
-export default async (amount: number, primaryAddress: string, lendingPoolContract: any, zDaiContract: any, web3State: any): Promise<number> => {
+export default async (amount: number, primaryAddress: string, lendingPoolContract: any, zDaiContract: any, web3State: any): Promise<any> => {
   await approveDai(lendingPoolContract, web3State, primaryAddress, amount);
   await mintZDai(lendingPoolContract, primaryAddress, amount);
   const balance = await zDaiContract.methods.balanceOf(primaryAddress).call();
-
-  return balance / globalDecimals;
+  return (parseFloat(balance) / globalDecimals);
 }
