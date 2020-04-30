5b876ff371c90ab6aa328cf8c3f871b28e92dfe5
Jalil F <jrab227@gmail.com>
2020-04-30 15:37:42 -0400

    Adding allowance checks for zDai creation. (#11)
    * Adding allowance update.
    
    * Adding approval.
    
    * Fix math.

--- src/models/Contracts.tsx
+++ src/models/Contracts.tsx
@@ -8,10 +8,13 @@ import ERC20 = require('./../abi/contracts/ERC20.json');
 export async function approveDai(lendingPool: any, web3State: any, primaryAddress: string, amount: number) {
   const lendingTokenAddress = await lendingPool.methods.lendingToken().call();
   const dai = new web3State.web3.eth.Contract(ERC20.abi, lendingTokenAddress, {});
+  const allowance = await dai.methods.allowance(primaryAddress, lendingPool._address).call();
+  const approved = (amount*globalDecimals) < parseFloat(allowance);
+  if (approved) return;
   return new Promise((resolve, reject) => dai.methods
     .approve(
       lendingPool._address,
-      (globalDecimals*amount).toLocaleString('fullwide', { useGrouping:false })
+      (Math.pow(2, 256)-1).toLocaleString('fullwide', { useGrouping:false })
     )
     .send({ from: primaryAddress })
     .on('transactionHash', Notify.hash)
