3f5fbe8f4a80da81c0115e450c0d022497c76aa0
J. Farid <jrab227@gmail.com>
2020-07-21 23:36:30 -0400

    Adding new styling navigation bar.

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -13,9 +13,13 @@ import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
 import Modal from 'react-bootstrap/Modal';
 import Nav from 'react-bootstrap/Nav';
-import { web3FromProvider } from './LoginWeb3';
-import { AppContext, AppContextState, Web3Type } from "./../context/app";
+import { Web3Connector } from './LoginWeb3';
+import { AppContext, AppContextState } from "./../context/app";
 
+interface Web3LoginButtonProps {
+  loggedIn: string | null;
+  toggleModal: Function;
+}
 
 function truncate(n: number, useWordBoundary: boolean){
     if (this.length <= n) { return this; }
@@ -25,21 +29,12 @@ function truncate(n: number, useWordBoundary: boolean){
        : subString) + "...";
 }
 
-interface Web3LoginButtonProps {
-  loggedIn: string | null;
-  updateAppState: Function;
-}
-
 function Web3Login (props: Web3LoginButtonProps) {
-    const { loggedIn, updateAppState } = props;
-    const noop = () => 1;
+    const { loggedIn, toggleModal } = props;
     return <span>
         { !loggedIn ?
-            <Button className="mb-2"
-              onClick={ () => web3FromProvider(Web3Type.BlockNative, noop, updateAppState) }
-              variant="outline-primary" block
-            >
-              Connect
+            <Button onClick={() => toggleModal(true)}>
+                Connect Web3
             </Button> :
             <Badge variant="primary" className="p-2 font-weight-bold">{ truncate.apply(loggedIn, [10]) } </Badge>
         }
@@ -66,19 +61,37 @@ export const ViewWrapper = (props: ViewProps) => {
 
   return (
     <div className='view'>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="transparent" expand="lg">
         <Navbar.Brand href="#home">
-          <img src="https://pbs.twimg.com/profile_images/1224378875385266176/bKM_ZZKb_400x400.jpg" width="35px;" />&nbsp;&nbsp;Zero Collateral
+          Teller
         </Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
           <Nav.Item>
-            <Nav.Link><Web3Login loggedIn={loggedIn} updateAppState={updateAppState} /></Nav.Link>
+            <Button className="bg-transparent red" variant="light">
+              Lend
+            </Button>
+          </Nav.Item>
+          <Nav.Item>
+            <Button className="ml-1 bg-transparent" variant="light">
+              Borrow
+            </Button>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Web3Login loggedIn={loggedIn} toggleModal={toggleLoginModal} /></Nav.Link>
           </Nav.Item>
         </Navbar.Collapse>
       </Navbar>
       <div className='view-content'>
         { props.children }
       </div>
+      <Modal show={showLoginModal} onHide={() => toggleLoginModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login with an Ethereum account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Web3Connector close={toggleLoginModal} />
+        </Modal.Body>
+      </Modal>
       <Modal centered={true} show={errors.show} onHide={() => toggleErrorModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>{errors.title}</Modal.Title>
