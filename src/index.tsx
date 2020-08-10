3fcadc1164ddb141bea4e436b8232da17ad7730f
Tanya Yakovleva <52566711+yakovleva-tanya@users.noreply.github.com>
2020-08-10 19:56:02 +0300

    Add favicon (#41)
    * add favicon
    
    * dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+import Helmet from "react-helmet";
 import Routing from "./components/Routing";
 import { AppContext, AppContextInterface } from './context/app';
 import useAppContext from './hooks/AppContext';
@@ -11,9 +11,21 @@ import './index.scss';
 function App() {
   const [state, updateAppState] = useAppContext();
   return (
-    <AppContext.Provider value={{state, updateAppState} as AppContextInterface}>
-      <Routing />
-    </AppContext.Provider>
+    <div>
+      <Helmet>
+        <link
+          rel="icon"
+          type="image/png"
+          href="assets/favicon.ico"
+          sizes="16x16"
+        />
+      </Helmet>
+      <AppContext.Provider
+        value={{ state, updateAppState } as AppContextInterface}
+      >
+        <Routing />
+      </AppContext.Provider>
+    </div>
   );
 }
 
