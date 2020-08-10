df3a239aadd85e3aa12f8454f317f7abf9bd2119
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-10 22:35:21 +0300

    add icons to currency selection

--- src/components/UI/CustomDropdown.tsx
+++ src/components/UI/CustomDropdown.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import dai from "../../../dist/assets/dai-logo.png";
+import usdc from "../../../dist/assets/usdc-logo.png";
+import usdt from "../../../dist/assets/usdt-logo.png";
+import "./custom-dropdown.scss";
+import dropdown from "../../../dist/assets/arrow-down.png";
 
 type dropdownProps = {
   selected: string;
@@ -14,25 +19,37 @@ export const CustomDropdown = ({
   return (
     <Dropdown>
       <Dropdown.Toggle
+        as={CustomToggle}
         variant="link"
         id="toggle"
-        className="dropdown font-medium p-1 text-gray"
+        className="dropdown font-medium p-1 text-gray dropdown-title dropdown-title d-flex flex-row align-items-center"
       >
-        {`${selected}   `}
+        <div className="mr-2">{`${selected}   `}</div>
+        {selected === "DAI" && <img src={dai} height="20" />}
+        {selected === "USDT" && <img src={usdt} height="20" />}
+        {selected === "USDC" && <img src={usdc} height="20" />}
       </Dropdown.Toggle>
-      <Dropdown.Menu>
+      <Dropdown.Menu className="dropdown-menu">
         {options.map((option: string) => {
-          return (
-            <Dropdown.Item
-              key={option}
-              eventKey={option}
-              onSelect={(e) => {
-                handleSelect(e);
-              }}
-            >
-              {option}
-            </Dropdown.Item>
-          );
+          if (option === selected) {
+            return;
+          } else {
+            return (
+              <Dropdown.Item
+                key={option}
+                eventKey={option}
+                onSelect={(e) => {
+                  handleSelect(e);
+                }}
+                className="dropdown-title d-flex flex-row align-items-center"
+              >
+                <div className="mr-2">{option}</div>
+                {option === "DAI" && <img src={dai} height="20" />}
+                {option === "USDT" && <img src={usdt} height="20" />}
+                {option === "USDC" && <img src={usdc} height="20" />}
+              </Dropdown.Item>
+            );
+          }
         })}
       </Dropdown.Menu>
     </Dropdown>
@@ -55,3 +72,18 @@ export const MockDropdown = ({ options }: customDropdownProps) => {
     />
   );
 };
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+    className="dropdown font-medium p-1 text-gray dropdown-title dropdown-title d-flex flex-row align-items-center"
+  >
+    {children}
+    <img height={8} src={dropdown} className="ml-3" />
+  </a>
+));
