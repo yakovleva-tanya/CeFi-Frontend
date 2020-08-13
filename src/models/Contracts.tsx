9b21c50f985b8a590ccc45b88313dfd85b6366e7
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-13 23:03:47 +0300

    fix formatting

--- src/models/Contracts.tsx
+++ src/models/Contracts.tsx
@@ -1,5 +1,5 @@
 import Notify from "./Web3Notify";
-import ERC20 = require("./../abi/contracts/ERC20Detailed.json");
+import ERC20 = require('./../abi/contracts/ERC20Detailed.json');
 
 async function getLendingToken(lendingPool: any, web3State: any) {
   const lendingTokenAddress = await lendingPool.methods.lendingToken().call();
@@ -9,79 +9,54 @@ async function getLendingToken(lendingPool: any, web3State: any) {
 /**
  * Redeeem zDai.
  */
-export async function redeemZDai(
-  amount: string,
-  primaryAddress: string,
-  lendingPool: any
-) {
-  return new Promise((resolve, reject) =>
-    lendingPool.methods
-      .withdraw(
-        parseFloat(amount).toLocaleString("fullwide", { useGrouping: false })
-      )
-      .send({ from: primaryAddress })
-      .on("transactionHash", Notify.hash)
-      .on("receipt", resolve)
-      .on("error", reject)
+export async function redeemZDai(amount: string, primaryAddress: string, lendingPool: any) {
+  return new Promise((resolve, reject) => lendingPool.methods
+    .withdraw(
+      (parseFloat(amount)).toLocaleString('fullwide', { useGrouping:false })
+    )
+    .send({ from: primaryAddress })
+    .on('transactionHash', Notify.hash)
+    .on('receipt', resolve)
+    .on('error', reject)
   );
 }
 
 /**
  * Approves spending of dai for zeroCollateral contracts.
  */
-export async function approveDai(
-  lendingPool: any,
-  web3State: any,
-  primaryAddress: string,
-  amount: number
-) {
+export async function approveDai(lendingPool: any, web3State: any, primaryAddress: string, amount: number) {
   const dai = await getLendingToken(lendingPool, web3State);
   const decimals = await dai.methods.decimals().call();
-  const tokenDecimals = 10 ** parseFloat(decimals);
-  const allowance = await dai.methods
-    .allowance(primaryAddress, lendingPool._address)
-    .call();
-  const approved = amount * tokenDecimals < parseFloat(allowance);
+  const tokenDecimals = 10**parseFloat(decimals);
+  const allowance = await dai.methods.allowance(primaryAddress, lendingPool._address).call();
+  const approved = (amount*tokenDecimals) < parseFloat(allowance);
   if (approved) return;
-  return new Promise((resolve, reject) =>
-    dai.methods
-      .approve(
-        lendingPool._address,
-        (amount * tokenDecimals).toLocaleString("fullwide", {
-          useGrouping: false,
-        })
-      )
-      .send({ from: primaryAddress })
-      .on("transactionHash", Notify.hash)
-      .on("receipt", resolve)
-      .on("error", reject)
+  return new Promise((resolve, reject) => dai.methods
+    .approve(
+      lendingPool._address,
+      (amount*tokenDecimals).toLocaleString('fullwide', { useGrouping:false })
+    )
+    .send({ from: primaryAddress })
+    .on('transactionHash', Notify.hash)
+    .on('receipt', resolve)
+    .on('error', reject)
   );
 }
 
-export async function mintZDai(
-  setProcessing: any,
-  contract: any,
-  web3State: any,
-  primaryAddress: string,
-  amount: number
-) {
+export async function mintZDai( setProcessing: any, contract: any, web3State: any, primaryAddress: string, amount: number) {
   const dai = await getLendingToken(contract, web3State);
   const decimals = await dai.methods.decimals().call();
-  const tokenDecimals = 10 ** parseFloat(decimals);
+  const tokenDecimals = 10**parseFloat(decimals);
   const onTransactionHash = (e: any) => {
     setProcessing(e);
     Notify.hash(e);
   };
-  return new Promise((resolve, reject) =>
-    contract.methods
-      .deposit(
-        (tokenDecimals * amount).toLocaleString("fullwide", {
-          useGrouping: false,
-        })
-      )
-      .send({ from: primaryAddress })
-      .on("transactionHash", onTransactionHash)
-      .on("receipt", resolve)
-      .on("error", reject)
+  return new Promise((resolve, reject) => contract.methods.deposit(
+      (tokenDecimals*amount).toLocaleString('fullwide', { useGrouping:false })
+    )
+    .send({ from: primaryAddress })
+    .on('transactionHash', onTransactionHash)
+    .on('receipt', resolve)
+    .on('error', reject)
   );
 }
