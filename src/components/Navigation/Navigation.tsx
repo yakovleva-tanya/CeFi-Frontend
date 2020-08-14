850a9ea1282488c8cf26c35b06fe84e02e76df8e
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-14 19:19:46 +0300

    remove commented out code

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,30 +3,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import "./navigation.scss";
 import teller_logo from "../../../dist/assets/teller_logo.svg";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
 import { NavLoginButton } from "./../LoginButton/LoginButton";
-
-type NavProps = {
-  to: string;
-  name: string;
-};
-
-// const NavigationLink = ({ to, name }: NavProps) => {
-//   return (
-//     <NavLink
-//       className="menu-button text-lg text-lightest-gray"
-//       activeClassName="active"
-//       to={`/${to}`}
-//     >
-//       {name}
-//     </NavLink>
-//   );
-// };
 
 const Navigation = () => {
   return (
@@ -47,12 +24,6 @@ const Navigation = () => {
         <div className="mb-4">
           <NavLoginButton />
         </div>
-        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end navbar-links">
-          <NavigationLink to="lend" name="Lend" />
-          <NavigationLink to="borrow" name="Borrow" />
-          <NavigationLink to="dashboard" name="Dashboard" />
-        </Navbar.Collapse> */}
       </Navbar>
     </Container>
   );
