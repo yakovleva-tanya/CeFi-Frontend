709b92e643df47a31f0c8f8d6f499202d7ee455e
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-21 02:03:13 +0300

    collateral percent component

--- src/components/Borrow/CollateralPercentSelection.tsx
+++ src/components/Borrow/CollateralPercentSelection.tsx
@@ -1,15 +1,42 @@
 import React, { useState, useContext } from "react";
 import { BorrowPageContext } from "../../context/borrowContext";
-import CustomInput from "../UI/CustomInput";
 import SubmenuCard from "../UI/SubmenuCard";
 import CustomSubmenuLink from "../UI/CustomSubmenuLink";
+import infoTriangle from "../../../dist/assets/info-triangle.svg";
+import Form from "react-bootstrap/Form";
 
 const CollateralPercentSubmenu = () => {
   const { borrowRequest, setBorrowRequest, setSubmenu } = useContext(
     BorrowPageContext
   );
   const [value, setValue] = useState(borrowRequest.collateralPercent);
-
+  const [warning, setWarning] = useState("");
+  const onChange = (e: any) => {
+    let value = e.target.value;
+    value = value.replace(/[^0-9.]/g, "");
+    value = Math.trunc(100 * parseFloat(value)) / 100;
+    if (isNaN(value)) {
+      value = "0.00";
+    }
+    e.target.value = value;
+    if (value > 150 || value < 0) {
+      setWarning("Please enter a number between 0-150");
+    } else {
+      setWarning("");
+    }
+    setValue(e.target.value);
+  };
+  const onSubmit = () => {
+    if (value > 150) {
+      setValue(150);
+    }
+    setValue(value);
+    setBorrowRequest({
+      ...borrowRequest,
+      collateralPercent: value,
+    });
+    setSubmenu(null);
+  };
   return (
     <SubmenuCard
       title="Collateral Percent"
@@ -17,45 +44,72 @@ const CollateralPercentSubmenu = () => {
         setSubmenu(null);
       }}
     >
-      <div className="mt-4 d-flex flex-column">
-        <CustomInput
-          onChangeFunction={(e: any) => {
-            setValue(parseInt(e.target.value) || 0);
+      <div className="-my-2 d-flex flex-column px-4">
+        {warning ? (
+          <FormValidationWarning message={warning} />
+        ) : (
+          <div style={{ height: "18px" }}>{""}</div>
+        )}
+        <input
+          className="input text-5xl font-medium text-black text-center"
+          value={value}
+          name="amount"
+          onChange={onChange}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
           }}
-          value={value.toString()}
-          type="number"
         />
-        <div className="text-lightest-gray text-lg ">%</div>
+        <Form>
+          <Form.Group controlId="formBasicRangeCustom" className="mb-0 mt-4">
+            <Form.Control
+              type="range"
+              custom
+              onChange={(e: any) =>
+                setValue(Math.trunc(100 * e.target.value * 1.5) / 100)
+              }
+              value={value / 1.5}
+            />
+          </Form.Group>
+        </Form>
+        <div className="text-xs text-gray d-flex justify-content-between mb-2">
+          <div>0%</div>
+          <div>150%</div>
+        </div>
         <div
           className="py-1 px-3 my-4 mx-auto border-thin pointer text-black"
-          onClick={() => {
-            setBorrowRequest({
-              ...borrowRequest,
-              collateralPercent: value,
-            });
-            setSubmenu(null);
-          }}
+          onClick={onSubmit}
         >
           Submit
         </div>
       </div>
     </SubmenuCard>
   );
-}
+};
 const CollateralPercentSelection = () => {
-  const { borrowRequest, setSubmenu } = useContext(
-    BorrowPageContext
-  );
+  const { borrowRequest, setSubmenu } = useContext(BorrowPageContext);
 
   return (
     <CustomSubmenuLink
       title={`${borrowRequest.collateralPercent}%`}
       onClickAction={() => {
-        setSubmenu(
-         <CollateralPercentSubmenu/>
-        );
+        setSubmenu(<CollateralPercentSubmenu />);
       }}
     />
   );
 };
 export default CollateralPercentSelection;
+
+type validationProps = {
+  message: string;
+};
+
+const FormValidationWarning = ({ message }: validationProps) => {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <img className="mr-1" src={infoTriangle} height={14} />
+      <div className="text-xs text-gray">{message}</div>
+    </div>
+  );
+};
