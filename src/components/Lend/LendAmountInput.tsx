bc6dd2e12687115849a8a8ec4651da6f82c607e0
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-20 17:53:22 +0300

    fix input format

--- src/components/Lend/LendAmountInput.tsx
+++ src/components/Lend/LendAmountInput.tsx
@@ -23,34 +23,31 @@ const LendAmountInput = ({ amount, handleChange }: lendAmountProps) => {
         selectedAmount
       )} ${selectedCurrency}`
     : "-";
-  const onBlur = (e: any) => {
-    e.target.value = e.target.value.replace(/[^0-9.]/g, "");
-    if (e.target.value.length < 1) {
-      e.target.value = 0;
+
+  const convertInputAmount = (value: any) => {
+    value = value.replace(/[^0-9.]/g, "");
+    value = parseFloat(value).toFixed(2);
+    if (isNaN(value)) {
+      value = "0.00";
     }
-    e.target.value = parseFloat(e.target.value).toFixed(2);
-    setSelectedAmount(e.target.value);
-    handleChange(e);
+    return value;
   };
+  const onBlur = (e: any) => {
+    e.target.value = convertInputAmount(e.target.value);
+    handleChange(e);
+    setSelectedAmount(e.target.value);
+  };
+  const onChange = (e: any) => {
+    handleChange(e);
+    const value = convertInputAmount(e.target.value);
+    setSelectedAmount(value);
+  };
+
   const value = `$${amount.toString().replace(/[^0-9.]/g, "")}`;
 
   return (
     <div className="">
-      <CustomInput
-        onChangeFunction={(e: any) => {
-          e.target.value = e.target.value.replace(/[^0-9.]/g, "");
-          handleChange(e);
-
-          if (e.target.value.length < 1) {
-            e.target.value = 0;
-          }
-
-          e.target.value = parseFloat(e.target.value).toFixed(2);
-          setSelectedAmount(e.target.value);
-        }}
-        value={value}
-        onBlur={onBlur}
-      />
+      <CustomInput onChangeFunction={onChange} value={value} onBlur={onBlur} />
       <div className="text-lightest-gray text-lg">{convertedAmount}</div>
     </div>
   );
