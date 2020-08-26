b19cea821c0e503b6088c452ce27be35d31d13df
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-26 18:07:18 +0300

    add loan details screen to repay subsection

--- src/components/Dashboard/BorrowRepaySubsection.tsx
+++ src/components/Dashboard/BorrowRepaySubsection.tsx
@@ -1,8 +1,8 @@
-import React from "React";
+import React, { useState } from "react";
 import TableRow from "../UI/TableRow";
 import BR from "../UI/BR";
 import CustomSubmenuLink from "../UI/CustomSubmenuLink";
-
+import PrimaryButton from "../UI/PrimaryButton";
 //TODO: get real values
 const LOANS = [
   {
@@ -84,6 +84,7 @@ const BorrowRepaySubsection = () => {
   const outstandingLoans: any = [];
   const repaidLoans: any = [];
   const currentTime = Date.now();
+  const [selectedLoan, setSelectedLoan] = useState(null);
 
   LOANS.map((loan) => {
     if (loan.amountOwed === 0) {
@@ -97,72 +98,117 @@ const BorrowRepaySubsection = () => {
 
   return (
     <div>
-      {outstandingLoans.length > 0 && (
-        <div className="mb-4">
-          <div className="text-left">Outstanding loans</div>
-          <div className="table border-thin mb-4 mt-3">
-            {outstandingLoans.map((loan: any) => {
-              return (
-                <div key={loan.due}>
-                  <TableRow
-                    title={`${Math.round(
-                      (loan.due - currentTime) / (60 * 60 * 24 * 1000)
-                    )} days remaining`}
-                  >
-                    <CustomSubmenuLink
-                      title={`${loan.loanSize} ${loan.lendWith}`}
-                      onClickAction={() => {}}
-                    />
-                  </TableRow>
-                  <BR />
-                </div>
-              );
-            })}
-          </div>
+      {!selectedLoan && (
+        <div>
+          {outstandingLoans.length > 0 && (
+            <div className="mb-4">
+              <div className="text-left">Outstanding loans</div>
+              <div className="table border-thin mb-4 mt-3">
+                {outstandingLoans.map((loan: any) => {
+                  return (
+                    <div key={loan.due}>
+                      <TableRow
+                        title={`${Math.round(
+                          (loan.due - currentTime) / (60 * 60 * 24 * 1000)
+                        )} days remaining`}
+                      >
+                        <CustomSubmenuLink
+                          title={`${loan.loanSize} ${loan.lendWith}`}
+                          onClickAction={() => {
+                            setSelectedLoan(loan);
+                          }}
+                        />
+                      </TableRow>
+                      <BR />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          {overdueLoans.length > 0 && (
+            <div className="mb-4">
+              <div className="text-left">Overdue loans</div>
+              <div className="table border-thin mb-4 mt-3">
+                {overdueLoans.map((loan: any) => {
+                  return (
+                    <div key={loan.due}>
+                      <TableRow
+                        title={`${Math.round(
+                          (currentTime - loan.due) / (60 * 60 * 24 * 1000)
+                        )} days overdue`}
+                      >
+                        <CustomSubmenuLink
+                          title={`${loan.loanSize} ${loan.lendWith}`}
+                          onClickAction={() => {
+                            setSelectedLoan(loan);
+                          }}
+                        />
+                      </TableRow>
+                      <BR />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          {repaidLoans.length > 0 && (
+            <div className="mb-4">
+              <div className="text-left">Repaid loans</div>
+              <div className="table border-thin mb-4 mt-3">
+                {repaidLoans.map((loan: any) => {
+                  return (
+                    <div key={loan.due}>
+                      <TableRow title={`ID ${loan.id}`}>
+                        <CustomSubmenuLink
+                          title={`${loan.loanSize} ${loan.lendWith}`}
+                          onClickAction={() => {
+                            setSelectedLoan(loan);
+                          }}
+                        />
+                      </TableRow>
+                      <BR />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
         </div>
       )}
-      {overdueLoans.length > 0 && (
-        <div className="mb-4">
-          <div className="text-left">Overdue loans</div>
-          <div className="table border-thin mb-4 mt-3">
-            {overdueLoans.map((loan: any) => {
-              return (
-                <div key={loan.due}>
-                  <TableRow
-                    title={`${Math.round(
-                      (currentTime - loan.due) / (60 * 60 * 24 * 1000)
-                    )} days overdue`}
-                  >
-                    <CustomSubmenuLink
-                      title={`${loan.loanSize} ${loan.lendWith}`}
-                      onClickAction={() => {}}
-                    />
-                  </TableRow>
-                  <BR />
-                </div>
-              );
-            })}
+      {selectedLoan && (
+        <div>
+          <div
+            className="border-thin pointer"
+            onClick={() => {
+              setSelectedLoan(null);
+            }}
+          >
+            Go Back
           </div>
-        </div>
-      )}
-      {repaidLoans.length > 0 && (
-        <div className="mb-4">
-          <div className="text-left">Repaid loans</div>
           <div className="table border-thin mb-4 mt-3">
-            {repaidLoans.map((loan: any) => {
-              return (
-                <div key={loan.due}>
-                  <TableRow title={`ID ${loan.id}`}>
-                    <CustomSubmenuLink
-                      title={`${loan.loanSize} ${loan.lendWith}`}
-                      onClickAction={() => {}}
-                    />
-                  </TableRow>
-                  <BR />
-                </div>
-              );
-            })}
+            <TableRow title="Interest Rate">
+              <div className="font-medium">{selectedLoan.interestRate}%</div>
+            </TableRow>
+            <BR />
+            <TableRow title="Loan size">
+              <div className="font-medium">
+                {selectedLoan.loanSize} {selectedLoan.lendWith}
+              </div>
+            </TableRow>
+            <BR />
           </div>
+
+          <div className="table border-thin mb-4 mt-3">
+            <TableRow title="Amount owed">
+              <div className="font-medium">
+                {selectedLoan.amountOwed} {selectedLoan.lendWith}
+              </div>
+            </TableRow>
+            <BR />
+          </div>
+
+          <PrimaryButton text="Repay Loan" onClick={() => {}} />
         </div>
       )}
     </div>
