dbabb0a843d89f9bdb932fad995ba3b61e34dcf3
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-08-27 18:19:58 +0300

    show selected loan id in repay card header

--- src/components/Dashboard/DashboardBorrowSection.tsx
+++ src/components/Dashboard/DashboardBorrowSection.tsx
@@ -7,9 +7,9 @@ const DashboardBorrowSection = () => {
   const { onPage, repayProcessState } = useContext(DashboardContext);
   const subSection = onPage.split("-")[1];
   let goBack: null | Function = null;
+  const { selectedLoan, setSelectedLoan } = repayProcessState;
 
   if (onPage === "Borrow-Repay") {
-    const { selectedLoan, setSelectedLoan } = repayProcessState;
     if (selectedLoan) {
       goBack = () => {
         setSelectedLoan(null);
@@ -18,7 +18,11 @@ const DashboardBorrowSection = () => {
   }
 
   return (
-    <Card className="main-card text-center" title={subSection} goBack={goBack}>
+    <Card
+      className="main-card text-center"
+      title={selectedLoan ? `ID ${selectedLoan.id}` : subSection}
+      goBack={goBack}
+    >
       <div className="my-5">
         {subSection == "Repay" && <BorrowRepaySubsection />}
         {subSection == "Deposit" && (
