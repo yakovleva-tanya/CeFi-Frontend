c759bfe12f482a001cfd21f217e2e389e96300b6
Tatiana Iakovleva <tiakovleva1@gmail.com>
2020-09-10 20:50:45 +0300

    borrow ux update, add supply validation warning, supply bug fix

--- src/components/Borrow/LoanSelectedCard.tsx
+++ src/components/Borrow/LoanSelectedCard.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Arrow from "../UI/Arrow";
+import compound from "../../../dist/assets/compound-full-logo.svg";
+import uniswap from "../../../dist/assets/uniswap-full-logo.svg";
 
 type loanSelectCardProps = {
   onClick: Function;
   title: string;
   subTitle: string;
   className?: string;
+  logos: Array<string>;
 };
 const LoanSelectCard = ({
   onClick,
   title,
   subTitle,
   className = "",
+  logos,
 }: loanSelectCardProps) => {
   return (
     <div
@@ -20,9 +24,22 @@ const LoanSelectCard = ({
         onClick();
       }}
     >
-      <div className="mr-4">
-        <div className="font-medium text-xl mb-1">{title}</div>
-        <div>{subTitle}</div>
+      <div className="mr-4 ml-2">
+        <div className="font-medium text-xl">{title}</div>
+        <div className="my-3">{subTitle}</div>
+        <div className="mb-1 text-lightest-gray font-weight-bold text-xs">
+          USE
+        </div>
+        <div className="d-flex flex-row">
+          {logos.map((logo: string) => (
+            <div key={logo}>
+              {logo === "compound" && (
+                <img src={compound} height={20} className="mr-3" />
+              )}
+              {logo === "uniswap" && <img src={uniswap} height={20} />}
+            </div>
+          ))}
+        </div>
       </div>
       <Arrow direction="right" />
     </div>
