1a35692015c5081e51f740e4c4914cc0c3a6a68e
Tanya <tiakovleva1@gmail.com>
2020-09-25 16:15:34 +0300

    comment out unused code

--- src/actions/Swap.tsx
+++ src/actions/Swap.tsx
@@ -1,2 +1,23 @@
-const Swap = async () => {};
-export default Swap;
+// const swap = async (
+// ) => {
+//   try {
+//     const network = web3State.network.toString();
+//     const address =
+//       network === "1"
+//         ? allContractAddresses.mainnet
+//         : allContractAddresses.ropsten;
+
+//     const data = web3UniswapContract.swapETHForExactTokens(amountOut, address, calldata_path, address_to, uint_deadline).encodeABI()
+
+//     let dappData = {
+//       location: address,
+//       data: data
+//     };
+//     const result = await escrow.callDapp(dappData);
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// export default swap;
