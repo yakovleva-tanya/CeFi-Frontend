46588d808ffd41879da11677b06c0ba98599cac7
Tanya <tiakovleva1@gmail.com>
2020-10-19 17:33:57 +0300

    remove unused components

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ const sslRedirect = () => {
 app.use(sslRedirect());
 app.use(express.static("dist"));
 
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "path/to/your/index.html"), function (err) {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
+
+app.get("*", function (req, res) {
+  res.sendFile("dist/index.html", {
+    root: path.join(__dirname, "../../client/build/"),
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
